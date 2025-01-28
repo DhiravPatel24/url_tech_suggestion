4cf34d3bc3bfc1d5ebc87aad5a6dc0ee1982024d
DhiravPatel24 <160713525+DhiravPatel24@users.noreply.github.com>
2025-01-28 14:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import requests
 import json
@@ -227,4 +228,5 @@ def get_company_info():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
